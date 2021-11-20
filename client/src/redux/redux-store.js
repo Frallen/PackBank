aeae77cf4575eb61e17b373dcbe2f334432f178c
Frallen <bladone439@gmail.com>
2021-11-20 19:10:55 +0300

    debugger

--- client/src/redux/redux-store.js
+++ client/src/redux/redux-store.js
@@ -1,4 +1,4 @@
-import { applyMiddleware, combineReducers, createStore } from "redux";
+import { applyMiddleware, combineReducers, compose, createStore } from "redux";
 import thunk from "redux-thunk";
 import AdminReducer from "./adminReducer";
 import AuthReducer from "./authReducer";
@@ -8,6 +8,13 @@ let reducers = combineReducers({
   admin: AdminReducer,
 });
 
-let store = createStore(reducers, applyMiddleware(thunk));
+// ДЛЯ РАСШИРЕНИЯ В ХРОМЕ
+const composeEnhancers =
+  process.env.NODE_ENV === "development"
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+    : compose;
+//////////
+
+let store = createStore(reducers, composeEnhancers(applyMiddleware(thunk)));
 
 export default store;
