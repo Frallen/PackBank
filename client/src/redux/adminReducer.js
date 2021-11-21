3994a29eb88e9da607fe4deb5b37884b2d02530f
Frallen <bladone439@gmail.com>
2021-11-21 23:07:52 +0300

    Банк,карты рабочее добавление удаление reducer

--- client/src/redux/adminReducer.js
+++ client/src/redux/adminReducer.js
@@ -3,6 +3,11 @@ import { Admin } from "../components/Api/api";
 const SubmitStart = "SubmitStart";
 const SubmitEnd = "SubmitEnd";
 const Error = "Error";
+const Clean = "Clean";
+const GetDataBank = "GetDataBank";
+const GetDataDebet = "GetDataDebet";
+const DeleteB = "DeleteB";
+const DeleteD = "DeleteD";
 
 let initialValues = {
   loading: false,
@@ -24,72 +29,125 @@ const AdminReducer = (state = initialValues, action) => {
         ...state,
         error: null,
         loading: false,
-        succ: action.succ,
-        data: action.data,
-        dataDebet: action.dataDebet,
+        succ: true,
       };
+    case GetDataBank:
+      //в имеющися массив data(будь он пустой или нет)
+      // копирую новый пришедший массив/объект и создаю новый
+      // получается в массив data засовываю новые данные
+      return { ...state, data: state.data.concat(action.data) };
+    case GetDataDebet:
+      return {
+        ...state,
+        dataDebet: state.dataDebet.concat(action.dataDebet),
+      };
+    //удалить банк
+    case DeleteB:
+      return {
+        ...state,
+        //  фильтрую массив,   возвращаю те элементы которые не равны удалленному id
+        //беру имеющиеся данные банка, проверяю каждый элемент и возвращаю те которые не равны удаленному id
+        data: state.data.filter((item) => item._id !== action.id),
+      };
+    //удалить дебет
+    case DeleteD:
+      return {
+        ...state,
+        //  фильтрую массив,   возвращаю те элементы которые не равны удалленному id
+        //беру имеющиеся данные банка, проверяю каждый элемент и возвращаю те которые не равны удаленному id
+        dataDebet: state.dataDebet.filter((item) => item._id !== action.id),
+      };
+
     case Error:
       return {
         ...state,
         error: action.error,
         loading: false,
       };
+    case Clean: {
+      return {
+        ...state,
+        data: null,
+      };
+    }
     default:
       return state;
   }
 };
+
+export const DeleteResp = () => ({ type: Clean });
 
 export default AdminReducer;
 //создать банк
 export const CreateBank = (data) => async (dispatch) => {
   dispatch({ type: SubmitStart });
   try {
-    await Admin.NewBank(data);
+    let snap = await Admin.NewBank(data);
 
-    dispatch({ type: SubmitEnd, succ: true });
+    dispatch({ type: SubmitEnd });
+    //получаю банк в виде объекта
+    dispatch({ type: GetDataBank, data: snap.data.bank });
   } catch (err) {
-    dispatch({ type: Error, error: err.response.data.message });
+    dispatch({ type: Error, error: err.message });
   }
 };
+
 //получить записи о всех банках
 export const GetBank = () => async (dispatch) => {
   dispatch({ type: SubmitStart });
   try {
     let snap = await Admin.GetBanks();
 
-    dispatch({ type: SubmitEnd, data: snap.data });
+    dispatch({ type: SubmitEnd });
+    dispatch({ type: GetDataBank, data: snap.data });
   } catch (err) {
-    dispatch({ type: Error, error: err.response.data.message });
+    dispatch({ type: Error, error: err.message });
   }
 };
 //удалить банк
 export const DeleteBank = (id) => async (dispatch) => {
   dispatch({ type: SubmitStart });
   try {
-    await Admin.DeleteBank(id);
-    dispatch({ type: SubmitEnd, succ: true });
+    let snap = await Admin.DeleteBank(id);
+
+    dispatch({ type: SubmitEnd });
+    dispatch({ type: DeleteB, id: snap.data.id });
   } catch (err) {
-    dispatch({ type: Error, error: err.response.data.message });
+    dispatch({ type: Error, error: err.message });
   }
 };
-
+//получить дебетовые карты
+export const GetDebetCards = () => async (dispatch) => {
+  dispatch({ type: SubmitStart });
+  try {
+    let snap = await Admin.GetALLDebet();
+    dispatch({ type: SubmitEnd });
+    dispatch({ type: GetDataDebet, dataDebet: snap.data });
+  } catch (err) {
+    dispatch({ type: Error, error: err.message });
+  }
+};
 //создать дебетовую карту
 export const CreateDebetCard = (data) => async (dispatch) => {
   dispatch({ type: SubmitStart });
   try {
-    await Admin.CreateDebet(data);
+    let snap = await Admin.CreateDebet(data);
     dispatch({ type: SubmitEnd, succ: true });
+    dispatch({ type: GetDataDebet, dataDebet: snap.data.card });
   } catch (err) {
-    dispatch({ type: Error, error: err.response.data.message });
+    dispatch({ type: Error, error: err.message });
   }
 };
-//получить дебетовые карты
-export const GetDebetCards =()=> async (dispatch) => {
+
+//удалить банк
+export const DeleteDebet = (id) => async (dispatch) => {
   dispatch({ type: SubmitStart });
   try {
-    let snap = await Admin.GetALLDebet();
-    dispatch({ type: SubmitEnd, dataDebet: snap.data });
+    let snap = await Admin.DeleteDebet(id);
+
+    dispatch({ type: SubmitEnd });
+    dispatch({ type: DeleteD, id: snap.data.id });
   } catch (err) {
-    dispatch({ type: Error, error: err.response.data.message });
+    dispatch({ type: Error, error: err.message });
   }
 };
