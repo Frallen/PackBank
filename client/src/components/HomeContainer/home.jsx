859d4c4b203a8bc25091494333e5760acbcb4a01
Frallen <bladone439@gmail.com>
2022-01-08 20:30:35 +0300

    prv

--- client/src/components/HomeContainer/home.jsx
+++ client/src/components/HomeContainer/home.jsx
@@ -1,20 +1,29 @@
-import React, { useState } from "react";
+import React from "react";
 import clas from "./home.module.scss";
-import video from "./../../etc/card2.mp4";
+import { NavLink } from "react-router-dom";
 let Home = () => {
   return (
-    <div className={clas.Main}>
-      <div className={clas.blocktext}>
-        <h1 className={clas.Title}>Shape.ru </h1>
-        <p className={clas.suptitle}> найдет, раскажет, поможет</p>
-        <p className={clas.Text}>
-          Ищете лучший банк? Условия? Лучший продукт или процент? Тогда Вам по
-          адресу.
-        </p>
+    <div className={clas.home}>
+      <h1 className={clas.home_title}>Подберем и найдем лучшее предложение</h1>
+      <div className={clas.home_first}>
+        <div className={clas.home_first_item}>
+          <h3 className={clas.home_first_item_title}>Побор кредита</h3>
+          <p className={clas.home_first_item_text}>
+            Подберём банки с высокой вероятностью одобрения
+          </p>
+          <NavLink to="/credit" className={clas.home_first_item_link}>Подобрать</NavLink>
+        </div>
+        <div className={clas.home_first_item}>
+          <h3 className={clas.home_first_item_title}>Подбор карты</h3>
+          <p className={clas.home_first_item_text}>
+            Подберём дебетову или кредитную карту
+          </p>
+          <div className={clas.home_first_item_spec}>
+            <NavLink to="/debit" className={clas.home_first_item_link}>Дебетовые</NavLink>
+            <NavLink to="/credit-card" className={clas.home_first_item_link}>Кредитные</NavLink>
+          </div>
+        </div>
       </div>
-      <video loop muted autoPlay className={clas.blockVideo}>
-        <source src={video} type="video/mp4"></source>
-      </video>
     </div>
   );
 };
